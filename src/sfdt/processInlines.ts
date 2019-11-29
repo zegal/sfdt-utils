12f1d4dfac11b14aead0df4b328871a56784e027
Kiran Kumar Chaudhary <kiran.chaudhary@dragonlaw.com.hk>
2019-11-29 10:28:50 +0545

    fix spacing

--- src/sfdt/processInlines.ts
+++ src/sfdt/processInlines.ts
@@ -1,28 +1,28 @@
-import get from 'lodash/get'
+import get from "lodash/get";
 
 // DEPRECATED, use sfdt/blocksProcess
 export default (sfdt, callback) => {
-	if (!sfdt.sections) {
-		console.warn('Missing: sfdt.sections', sfdt)
-		return false
-	}
+  if (!sfdt.sections) {
+    console.warn('Missing: sfdt.sections', sfdt);
+    return false;
+  }
 
-	sfdt.sections.forEach((section) => {
-		if (!section.blocks && !section.headersFooters) {
-			return false
-		}
+  sfdt.sections.forEach(section => {
+    if (!section.blocks && !section.headersFooters) {
+      return false;
+    }
 
-		section.blocks.forEach((block) => {
-			if (!block.inlines) {
-				return false
-			}
+    section.blocks.forEach(block => {
+      if (!block.inlines) {
+        return false;
+      }
 
-			block.inlines = callback(block.inlines)
-		})
+      block.inlines = callback(block.inlines);
+    });
 
-		if (get(section, 'headersFooters')) {
-			for (let eachKey in section.headersFooters) {
-      	const child = section.headersFooters[eachKey];
+    if (get(section, 'headersFooters')) {
+      for (let eachKey in section.headersFooters) {
+        const child = section.headersFooters[eachKey];
 
         if (child.blocks) {
           child.blocks.forEach(block => {
@@ -34,7 +34,7 @@ export default (sfdt, callback) => {
         }
       }
     }
-	})
+  });
 
-	return true
-}
+  return true;
+};
