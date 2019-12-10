8c360d2a89feeab9fd352d11a23b68149b3a42df
Kiran Kumar Chaudhary <kiran.chaudhary@dragonlaw.com.hk>
2019-12-10 11:52:18 +0545

    fix for conditional test

--- src/sfdt/__tests__/fixtures/bookmarkEndingInMultipleInlineSfdt.js
+++ src/sfdt/__tests__/fixtures/bookmarkEndingInMultipleInlineSfdt.js
@@ -10,47 +10,47 @@
  */
 const firstInline = [
   {
-      "text": ""
+    text: ''
   },
   {
-      "characterFormat": {
-          "highlightColor": ""
-      },
-      "bookmarkType": 0,
-      "name": "COND::9e7d0dc1-b9ed-4baa-9399-a4c4c9be96d4"
+    characterFormat: {
+      highlightColor: ''
+    },
+    bookmarkType: 0,
+    name: 'COND::9e7d0dc1-b9ed-4baa-9399-a4c4c9be96d4'
   },
   {
-    "hasFieldEnd": true,
-    "fieldType": 0
+    hasFieldEnd: true,
+    fieldType: 0
   },
   {
-      "characterFormat": {
-          "highlightColor": ""
-      },
-      "bookmarkType": 0,
-      "name": "COND::410a27f0-117d-491d-ba28-48cf2ba058f1"
+    characterFormat: {
+      highlightColor: ''
+    },
+    bookmarkType: 0,
+    name: 'COND::410a27f0-117d-491d-ba28-48cf2ba058f1'
   },
   {
-      "hasFieldEnd": true,
-      "fieldType": 0
+    hasFieldEnd: true,
+    fieldType: 0
   },
   {
-      "characterFormat": {
-          "highlightColor": ""
-      },
-      "text": "This is first paragraph."
+    characterFormat: {
+      highlightColor: ''
+    },
+    text: 'This is first paragraph.'
   },
   {
-      "fieldType": 2
+    fieldType: 1
   },
   {
-      "characterFormat": {
-          "highlightColor": ""
-      },
-      "bookmarkType": 1,
-      "name": "COND::410a27f0-117d-491d-ba28-48cf2ba058f1"
+    characterFormat: {
+      highlightColor: ''
+    },
+    bookmarkType: 1,
+    name: 'COND::410a27f0-117d-491d-ba28-48cf2ba058f1'
   }
-]
+];
 
 /**
  * Conditional bookmark which start in firstInline end here
@@ -58,87 +58,87 @@ const firstInline = [
  */
 const lastInline = [
   {
-      "characterFormat": {
-          "highlightColor": ""
-      },
-      "text": "This is third paragraph."
+    characterFormat: {
+      highlightColor: ''
+    },
+    text: 'This is third paragraph.'
   },
   {
-      "fieldType": 2
+    fieldType: 1
   },
   {
-      "characterFormat": {
-          "highlightColor": ""
-      },
-      "bookmarkType": 1,
-      "name": "COND::9e7d0dc1-b9ed-4baa-9399-a4c4c9be96d4"
+    characterFormat: {
+      highlightColor: ''
+    },
+    bookmarkType: 1,
+    name: 'COND::9e7d0dc1-b9ed-4baa-9399-a4c4c9be96d4'
   }
-]
+];
 
 const sfdt = {
-  "sections": [
-      {
-          "sectionFormat": {
-              "pageWidth": 612,
-              "pageHeight": 792,
-              "leftMargin": 72,
-              "rightMargin": 72,
-              "topMargin": 72,
-              "bottomMargin": 72,
-              "differentFirstPage": false,
-              "differentOddAndEvenPages": false,
-              "headerDistance": 36,
-              "footerDistance": 36,
-              "bidi": false
+  sections: [
+    {
+      sectionFormat: {
+        pageWidth: 612,
+        pageHeight: 792,
+        leftMargin: 72,
+        rightMargin: 72,
+        topMargin: 72,
+        bottomMargin: 72,
+        differentFirstPage: false,
+        differentOddAndEvenPages: false,
+        headerDistance: 36,
+        footerDistance: 36,
+        bidi: false
+      },
+      blocks: [
+        {
+          paragraphFormat: {
+            styleName: 'Normal'
           },
-          "blocks": [
-              {
-                  "paragraphFormat": {
-                      "styleName": "Normal"
-                  },
-                  "inlines": firstInline
+          inlines: firstInline
+        },
+        {
+          paragraphFormat: {
+            styleName: 'Normal'
+          },
+          characterFormat: {
+            highlightColor: ''
+          },
+          inlines: [
+            {
+              characterFormat: {
+                highlightColor: ''
               },
-              {
-                  "paragraphFormat": {
-                      "styleName": "Normal"
-                  },
-                  "characterFormat": {
-                      "highlightColor": ""
-                  },
-                  "inlines": [
-                      {
-                          "characterFormat": {
-                              "highlightColor": ""
-                          },
-                          "text": "This is second paragraph."
-                      }
-                  ]
-              },
-              {
-                  "paragraphFormat": {
-                      "styleName": "Normal"
-                  },
-                  "characterFormat": {
-                      "highlightColor": ""
-                  },
-                  "inlines": lastInline
-              },
-              {
-                  "paragraphFormat": {
-                      "styleName": "Normal"
-                  },
-                  "inlines": [
-                      {
-                          "characterFormat": {
-                              "bidi": false
-                          },
-                          "text": "Keep Distance with me."
-                      }
-                  ]
-              }
+              text: 'This is second paragraph.'
+            }
           ]
-      }
+        },
+        {
+          paragraphFormat: {
+            styleName: 'Normal'
+          },
+          characterFormat: {
+            highlightColor: ''
+          },
+          inlines: lastInline
+        },
+        {
+          paragraphFormat: {
+            styleName: 'Normal'
+          },
+          inlines: [
+            {
+              characterFormat: {
+                bidi: false
+              },
+              text: 'Keep Distance with me.'
+            }
+          ]
+        }
+      ]
+    }
   ]
-}
+};
 
-export default sfdt
+export default sfdt;
