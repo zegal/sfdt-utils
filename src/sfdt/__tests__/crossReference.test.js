a9118c6641880effc2d194a507b67274a6cc23f5
ankitaShrma <ankita.sharma@zegal.com>
2020-01-16 15:06:31 +0545

    Separate the ref update from sfdt processing

--- src/sfdt/__tests__/crossReference.test.js
+++ src/sfdt/__tests__/crossReference.test.js
@@ -1,16 +1,16 @@
-import {findAnchorAndUpdate, manipulateSfdtForCrossRef} from '../crossReference';
+import updateRef from '../updateRef';
 
 import crossRefSfdt from './fixtures/crossRefSfdt';
 
 describe('crossReference', () => {
 	it('findAnchorAndUpdate', () => {
 		const originalSfdt = crossRefSfdt;
-		let sfdt = findAnchorAndUpdate(crossRefSfdt, manipulateSfdtForCrossRef);
+		let sfdt = updateRef(crossRefSfdt);
 		// Delete a list block (array position 5)
 		const blocks = originalSfdt.sections[0].blocks;
 		originalSfdt.sections[0].blocks = blocks.slice(0, 5).concat(blocks.slice(6, blocks.length));
 
-		let newSfdt = findAnchorAndUpdate(originalSfdt, manipulateSfdtForCrossRef);
+		let newSfdt = updateRef(originalSfdt);
 
 		expect(newSfdt.sections[0].blocks[0].inlines[8].text).toBe('4.1');
 	});
