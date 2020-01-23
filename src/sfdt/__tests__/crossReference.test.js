6d85533b96dc209293a29c2b014741b77df4bf30
ankitaShrma <ankita.sharma@zegal.com>
2020-01-24 02:18:35 +0545

    Get the nested number formats of the child listlevel

--- src/sfdt/__tests__/crossReference.test.js
+++ src/sfdt/__tests__/crossReference.test.js
@@ -1,11 +1,11 @@
 import updateRef from '../updateRef';
 
 import crossRefSfdt from './fixtures/crossRefSfdt';
+import multicf from './fixtures/nestedCrossRef';
 
 describe('crossReference', () => {
 	it('findAnchorAndUpdate', () => {
 		const originalSfdt = crossRefSfdt;
-		let sfdt = updateRef(crossRefSfdt);
 		// Delete a list block (array position 5)
 		const blocks = originalSfdt.sections[0].blocks;
 		originalSfdt.sections[0].blocks = blocks.slice(0, 5).concat(blocks.slice(6, blocks.length));
@@ -13,5 +13,13 @@ describe('crossReference', () => {
 		let newSfdt = updateRef(originalSfdt);
 
 		expect(newSfdt.sections[0].blocks[0].inlines[8].text).toBe('4.1');
+	});
+
+	it('findAnchorAndUpdate multiple nested list condition', () => {
+		const originalSfdt = multicf;
+
+		let newSfdt = updateRef(originalSfdt);
+
+		expect('4.1').toBe('4.1');
 	});
 });
