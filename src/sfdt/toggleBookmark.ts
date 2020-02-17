66e57e291306bddb21bd80b09128961a1895d633
Anthony <anthony.michel@zegal.com>
2020-02-17 09:40:43 +0800

    Applying HC's new condition logic

--- src/sfdt/toggleBookmark.ts
+++ src/sfdt/toggleBookmark.ts
@@ -22,113 +22,125 @@ import Stack from '../stack';
  *
  * @returns {Object} updatedSFDT
  */
-const toggleBookmark = (sfdt: any, name: string, toggleOn = true) => {
-	// console.log('toggleBookmark name', name)
-	// console.log('toggleBookmark mode', toggleOn ? 'on' : 'off')
-
-	if (toggleOn) {
-		// toggle field on
-		const processInlines = (inlines) => {
-			const newInlines: any[] = [];
-
-			forEach(inlines, (inline, index: any) => {
-				let defaultAdd = true;
-				const nextInline = inlines[index + 1];
-				const prevInline = inlines[index - 1];
-
-				// console.log("FOUND, DOING TOGGLE ON");
-				// don't add in newInlines if inline is toggleEnd inline object
-				// and nextInline is matching bookmark
-				// i.e fieldType is 2 or hasFieldEnd is true
-				if (isToggleEnd(inline) && isMatchingBookmark(nextInline, name)) {
-					defaultAdd = false;
-				}
-
-				if (isToggleStart(inline) && isMatchingBookmark(prevInline, name)) {
-					defaultAdd = false;
-				}
-
-				if (defaultAdd) {
-					newInlines.push(inline);
-				}
-			});
-
-			return newInlines;
-		};
-
-		process(sfdt, processInlines);
+function toggleBookmark(sfdt, name, toggleOn = true) {
+	if(toggleOn) {
+	  // do nothing
 	} else {
-		const stack = new Stack();
-		let isStackContainInline = false;
-
-		// toggle field off
-		const processInlines = (inlines) => {
-			const newInlines = filter(inlines, (inline) => {
-				if (isMatchingBookmark(inline, name) && isConditionalBookmark(inline)) {
-					if (isBookmarkStart(inline)) {
-						stack.push(inline);
-						isStackContainInline = true;
-
-						return false;
-					}
-
-					if (isBookmarkEnd(inline)) {
-						stack.pop();
-						isStackContainInline = false;
-						return false;
-					}
-				}
-
-				if (!stack.isEmpty()) {
-					return false;
-				}
-				return true;
-			});
-
-			return newInlines;
-		};
-
-		const processListBlock = (block) => {
-			const normalizedBlock = normalizeBlockInlines(block);
-			const doWeNeedListCondition = canUseListCondition(normalizedBlock, name);
-			if (doWeNeedListCondition) {
-				if (conditionStartEndInSameInlines(normalizedBlock, name)) {
-					return false;
-				}
-
-				if (conditionStartInFirstInlines(normalizedBlock, name)) {
-					stack.push(block);
-
-					return false;
-				}
-
-				if (conditionEndInSameLastInlines(normalizedBlock, name)) {
-					stack.pop();
-
-					return false;
-				}
-
-				if (!stack.isEmpty()) {
-					return false;
-				}
-
-				return true;
-			}
-
-			if (!stack.isEmpty()) {
-				if (isStackContainInline || !doWeNeedListCondition) {
-					processInlines(block.inlines);
-				}
-				return false;
-			}
-
-			return true;
-		};
-
-		process(sfdt, processInlines, processListBlock);
+	  sfdt.sections.forEach(section => processSection(section, name, {}));
 	}
-
 	return sfdt;
-};
+  }
+  
+  
+  function processSection(section, condition, options = {}) {
+	//console.log('processing section for condition: ', condition)
+	for(let i = 0; i < section.blocks.length;) {
+	  if(!processBlock(section.blocks, i, condition, options)) i++;
+	}
+  }
+  
+  /*
+	processParagraph for deletion
+	-- a paragraph is made up of inlines[]
+	-- if a bookmark spans the first and last inline element, remove the entire paragraph
+	-- if a bookmark spans witihn inline elements, just remove those elements inclusive
+	-- if a bookmark spans multiple paragraphs, collapse
+  \*/
+  function processParagraph(paraBlock, condition, options = {}, inTable = false) {
+	//console.log("processParagraph", condition, options, `inTable: ${inTable}`);
+	let processedDelete = false;
+	for(let i = 0; i < paraBlock.inlines.length;) {
+	  let inline = paraBlock.inlines[i];
+	  if(inline.name == condition) {
+		if(inline.bookmarkType == 0) {
+			//console.log("FOUND START CONDITION", condition, paraBlock)
+		  options.withinDeleteContext = true;
+		  inline.markDelete = true;
+		} else if(inline.bookmarkType == 1) {
+			//console.log("FOUND END CONDITION", condition, paraBlock)
+		  delete options.withinDeleteContext;
+		  inline.markDelete = true;
+		}
+	  } else {
+		if(options.withinDeleteContext) {
+		  inline.markDelete = true;
+		}
+	  }
+	  if(inline.markDelete) {
+		paraBlock.inlines.splice(i, 1);
+		processedDelete = true;
+	  } else {
+		i++;
+	  }
+	}
+	return processedDelete;
+  }
+  
+  /*
+	processTable for delete
+	-- if a bookmark is within a cell, use processParagraph
+	-- if a bookmark spans multiple cells
+	--   if it spans the first and last cell in one row, then REMOVE THE ENTIRE ROW
+	--   if it spans across cells in one row, then REMOVE THE ENTIRE ROW
+	--   if it spans multiple rows, remove the rows affected.  ALL ROWS
+  */
+  function processTable(tableBlock, condition, options = {}) {
+	//console.log("processTable", condition, options);
+	let startDeleteRow, endDeleteRow;
+	tableBlock.rows.forEach((row, i) => {
+	  row.cells.forEach((cell, j) => {
+		for(let k = 0; k < cell.blocks.length;) {
+		  if(!processBlock(cell.blocks, k, condition, options, true)) {
+			k++;
+		  } else {
+			cell.blocks.splice(k, 1)
+		  }
+		}
+  
+		if(options.withinDeleteContext) {
+		  if(!startDeleteRow) {
+			startDeleteRow = i;
+		  }
+		} else {
+		  if(startDeleteRow >= 0 && !(endDeleteRow >= 0) ) {
+			endDeleteRow = i;
+		  }
+		}
+	  })
+	})
+  
+	
+	if(startDeleteRow >= 0) {
+		if(!(endDeleteRow >= 0)) endDeleteRow = tableBlock.rows.length - 1;
+	  console.log("table, deleting rows: ", startDeleteRow, endDeleteRow)
+	  tableBlock.rows.splice(startDeleteRow, endDeleteRow - startDeleteRow + 1);
+	}
+  
+	if(tableBlock.rows.length == 0) {
+	  // if no more rows, return true to parent to have parent delete block
+	  return true;
+	}
+	
+  }
+  
+  function processBlock(blocks, i, condition, options, inTable = false) {
+	  // console.log("processBlock", condition, options)
+	  let block = blocks[i];
+	  if(block.rows) {
+		if(processTable(block, condition, options)) {
+		  blocks.splice(i, 1);
+		  return true;
+		}
+	  } else {
+		if(processParagraph(block, condition, options, inTable)) {
+		  // if inlines is empty, remove block
+		  if(block.inlines.length == 0) {
+			blocks.splice(i, 1);
+			return true;
+		  }
+		}
+	  }
+  }
+  
 
 export default toggleBookmark;
