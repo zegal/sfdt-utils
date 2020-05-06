cf90e3199dd0bf44916f5b00e2e7a7507b02e28e
kiran_bhatt <kiran.bhatt@dragonlaw.com.hk>
2020-05-06 14:22:02 +0545

    Added new test for nested table population

---
+++ src/sfdt/__tests__/fixtures/nestedTableInlines.js
@@ -0,0 +1,736 @@
+module.exports = {
+	sections: [
+		{
+			sectionFormat: {
+				pageWidth: 612,
+				pageHeight: 792,
+				leftMargin: 72,
+				rightMargin: 72,
+				topMargin: 72,
+				bottomMargin: 72,
+				differentFirstPage: false,
+				differentOddAndEvenPages: false,
+				headerDistance: 36,
+				footerDistance: 36,
+				bidi: false
+			},
+			blocks: [
+				{
+					rows: [
+						{
+							cells: [
+								{
+									blocks: [
+										{
+											paragraphFormat: {
+												styleName: 'Normal',
+												listFormat: {}
+											},
+											characterFormat: {},
+											inlines: [
+												{
+													characterFormat: {
+														highlightColor: 'NoColor'
+													},
+													bookmarkType: 0,
+													name: 'DATA::dcb6ec7f-715d-47e8-b1f5-4b7c9ea279ad::name'
+												},
+												{
+													characterFormat: {
+														highlightColor: 'NoColor',
+														bidi: false
+													},
+													text: 'name'
+												},
+												{
+													characterFormat: {
+														highlightColor: 'NoColor'
+													},
+													bookmarkType: 1,
+													name: 'DATA::dcb6ec7f-715d-47e8-b1f5-4b7c9ea279ad::name'
+												}
+											]
+										}
+									],
+									cellFormat: {
+										borders: {
+											top: {},
+											left: {},
+											right: {},
+											bottom: {},
+											diagonalDown: {},
+											diagonalUp: {},
+											horizontal: {},
+											vertical: {}
+										},
+										shading: {},
+										preferredWidth: 234,
+										cellWidth: 234,
+										columnSpan: 1,
+										rowSpan: 1
+									},
+									columnIndex: 0
+								},
+								{
+									blocks: [
+										{
+											rows: [
+												{
+													cells: [
+														{
+															blocks: [
+																{
+																	paragraphFormat: {
+																		styleName: 'Normal',
+																		listFormat: {}
+																	},
+																	characterFormat: {},
+																	inlines: [
+																		{
+																			characterFormat: {},
+																			bookmarkType: 0,
+																			name:
+																				'DATA::dce52b7a-5a39-487e-b1b3-1dedbac18f5f::name'
+																		},
+																		{
+																			characterFormat: {
+																				highlightColor: '#FFC0CB',
+																				bidi: false
+																			},
+																			text: 'name'
+																		},
+																		{
+																			characterFormat: {},
+																			bookmarkType: 1,
+																			name:
+																				'DATA::dce52b7a-5a39-487e-b1b3-1dedbac18f5f::name'
+																		}
+																	]
+																}
+															],
+															cellFormat: {
+																borders: {
+																	top: {},
+																	left: {},
+																	right: {},
+																	bottom: {},
+																	diagonalDown: {},
+																	diagonalUp: {},
+																	horizontal: {},
+																	vertical: {}
+																},
+																shading: {},
+																preferredWidth: 111.6,
+																cellWidth: 111.6,
+																columnSpan: 1,
+																rowSpan: 1
+															},
+															columnIndex: 0
+														},
+														{
+															blocks: [
+																{
+																	rows: [
+																		{
+																			cells: [
+																				{
+																					blocks: [
+																						{
+																							paragraphFormat: {
+																								styleName: 'Normal',
+																								listFormat: {}
+																							},
+																							characterFormat: {},
+																							inlines: [
+																								{
+																									characterFormat: {},
+																									bookmarkType: 0,
+																									name:
+																										'DATA::24c243c0-1b01-453f-992e-3c0d605e471a::name'
+																								},
+																								{
+																									characterFormat: {
+																										highlightColor:
+																											'#FFC0CB',
+																										bidi: false
+																									},
+																									text: 'name'
+																								},
+																								{
+																									characterFormat: {},
+																									bookmarkType: 1,
+																									name:
+																										'DATA::24c243c0-1b01-453f-992e-3c0d605e471a::name'
+																								}
+																							]
+																						}
+																					],
+																					cellFormat: {
+																						borders: {
+																							top: {},
+																							left: {},
+																							right: {},
+																							bottom: {},
+																							diagonalDown: {},
+																							diagonalUp: {},
+																							horizontal: {},
+																							vertical: {}
+																						},
+																						shading: {},
+																						preferredWidth: 50.39999999999999,
+																						cellWidth: 50.39999999999999,
+																						columnSpan: 1,
+																						rowSpan: 1
+																					},
+																					columnIndex: 0
+																				},
+																				{
+																					blocks: [
+																						{
+																							paragraphFormat: {
+																								styleName: 'Normal',
+																								listFormat: {}
+																							},
+																							characterFormat: {},
+																							inlines: []
+																						}
+																					],
+																					cellFormat: {
+																						borders: {
+																							top: {},
+																							left: {},
+																							right: {},
+																							bottom: {},
+																							diagonalDown: {},
+																							diagonalUp: {},
+																							horizontal: {},
+																							vertical: {}
+																						},
+																						shading: {},
+																						preferredWidth: 50.39999999999999,
+																						cellWidth: 50.39999999999999,
+																						columnSpan: 1,
+																						rowSpan: 1
+																					},
+																					columnIndex: 1
+																				}
+																			],
+																			rowFormat: {
+																				height: 0,
+																				heightType: 'Auto',
+																				borders: {
+																					top: {},
+																					left: {},
+																					right: {},
+																					bottom: {},
+																					diagonalDown: {},
+																					diagonalUp: {},
+																					horizontal: {},
+																					vertical: {}
+																				},
+																				gridBefore: 0,
+																				gridAfter: 0
+																			}
+																		}
+																	],
+																	grid: [50.39999999999999, 50.39999999999999],
+																	tableFormat: {
+																		borders: {
+																			top: {
+																				lineStyle: 'Single',
+																				lineWidth: 0.5
+																			},
+																			left: {
+																				lineStyle: 'Single',
+																				lineWidth: 0.5
+																			},
+																			right: {
+																				lineStyle: 'Single',
+																				lineWidth: 0.5
+																			},
+																			bottom: {
+																				lineStyle: 'Single',
+																				lineWidth: 0.5
+																			},
+																			diagonalDown: {},
+																			diagonalUp: {},
+																			horizontal: {
+																				lineStyle: 'Single',
+																				lineWidth: 0.5
+																			},
+																			vertical: {
+																				lineStyle: 'Single',
+																				lineWidth: 0.5
+																			}
+																		},
+																		shading: {},
+																		topMargin: 0,
+																		rightMargin: 5.4,
+																		leftMargin: 5.4,
+																		bottomMargin: 0,
+																		preferredWidthType: 'Auto'
+																	},
+																	columnCount: 2
+																},
+																{
+																	paragraphFormat: {
+																		styleName: 'Normal',
+																		listFormat: {}
+																	},
+																	characterFormat: {},
+																	inlines: []
+																}
+															],
+															cellFormat: {
+																borders: {
+																	top: {},
+																	left: {},
+																	right: {},
+																	bottom: {},
+																	diagonalDown: {},
+																	diagonalUp: {},
+																	horizontal: {},
+																	vertical: {}
+																},
+																shading: {},
+																preferredWidth: 111.6,
+																cellWidth: 111.6,
+																columnSpan: 1,
+																rowSpan: 1
+															},
+															columnIndex: 1
+														}
+													],
+													rowFormat: {
+														height: 0,
+														heightType: 'Auto',
+														borders: {
+															top: {},
+															left: {},
+															right: {},
+															bottom: {},
+															diagonalDown: {},
+															diagonalUp: {},
+															horizontal: {},
+															vertical: {}
+														},
+														gridBefore: 0,
+														gridAfter: 0
+													}
+												}
+											],
+											grid: [111.6, 111.6],
+											tableFormat: {
+												borders: {
+													top: {
+														lineStyle: 'Single',
+														lineWidth: 0.5
+													},
+													left: {
+														lineStyle: 'Single',
+														lineWidth: 0.5
+													},
+													right: {
+														lineStyle: 'Single',
+														lineWidth: 0.5
+													},
+													bottom: {
+														lineStyle: 'Single',
+														lineWidth: 0.5
+													},
+													diagonalDown: {},
+													diagonalUp: {},
+													horizontal: {
+														lineStyle: 'Single',
+														lineWidth: 0.5
+													},
+													vertical: {
+														lineStyle: 'Single',
+														lineWidth: 0.5
+													}
+												},
+												shading: {},
+												topMargin: 0,
+												rightMargin: 5.4,
+												leftMargin: 5.4,
+												bottomMargin: 0,
+												preferredWidthType: 'Auto'
+											},
+											columnCount: 2
+										},
+										{
+											paragraphFormat: {
+												styleName: 'Normal',
+												listFormat: {}
+											},
+											characterFormat: {},
+											inlines: []
+										}
+									],
+									cellFormat: {
+										borders: {
+											top: {},
+											left: {},
+											right: {},
+											bottom: {},
+											diagonalDown: {},
+											diagonalUp: {},
+											horizontal: {},
+											vertical: {}
+										},
+										shading: {},
+										preferredWidth: 234,
+										cellWidth: 234,
+										columnSpan: 1,
+										rowSpan: 1
+									},
+									columnIndex: 1
+								}
+							],
+							rowFormat: {
+								height: 0,
+								heightType: 'Auto',
+								borders: {
+									top: {},
+									left: {},
+									right: {},
+									bottom: {},
+									diagonalDown: {},
+									diagonalUp: {},
+									horizontal: {},
+									vertical: {}
+								},
+								gridBefore: 0,
+								gridAfter: 0
+							}
+						}
+					],
+					grid: [234, 234],
+					tableFormat: {
+						borders: {
+							top: {
+								lineStyle: 'Single',
+								lineWidth: 0.5
+							},
+							left: {
+								lineStyle: 'Single',
+								lineWidth: 0.5
+							},
+							right: {
+								lineStyle: 'Single',
+								lineWidth: 0.5
+							},
+							bottom: {
+								lineStyle: 'Single',
+								lineWidth: 0.5
+							},
+							diagonalDown: {},
+							diagonalUp: {},
+							horizontal: {
+								lineStyle: 'Single',
+								lineWidth: 0.5
+							},
+							vertical: {
+								lineStyle: 'Single',
+								lineWidth: 0.5
+							}
+						},
+						shading: {},
+						topMargin: 0,
+						rightMargin: 5.4,
+						leftMargin: 5.4,
+						bottomMargin: 0,
+						preferredWidthType: 'Auto'
+					},
+					columnCount: 2
+				},
+				{
+					paragraphFormat: {
+						styleName: 'Normal',
+						listFormat: {}
+					},
+					characterFormat: {},
+					inlines: [
+						{
+							characterFormat: {},
+							text: ''
+						}
+					]
+				}
+			],
+			headersFooters: {
+				header: {
+					blocks: [
+						{
+							paragraphFormat: {
+								listFormat: {}
+							},
+							characterFormat: {},
+							inlines: []
+						}
+					]
+				},
+				footer: {
+					blocks: [
+						{
+							paragraphFormat: {
+								listFormat: {}
+							},
+							characterFormat: {},
+							inlines: []
+						}
+					]
+				}
+			}
+		}
+	],
+	characterFormat: {
+		bold: false,
+		italic: false,
+		fontSize: 11,
+		fontFamily: 'Calibri',
+		underline: 'None',
+		strikethrough: 'None',
+		baselineAlignment: 'Normal',
+		highlightColor: 'NoColor',
+		fontColor: '#000000',
+		fontSizeBidi: 11,
+		fontFamilyBidi: 'Calibri'
+	},
+	paragraphFormat: {
+		leftIndent: 0,
+		rightIndent: 0,
+		firstLineIndent: 0,
+		textAlignment: 'Left',
+		beforeSpacing: 0,
+		afterSpacing: 0,
+		lineSpacing: 1.0791667,
+		lineSpacingType: 'Multiple',
+		listFormat: {},
+		bidi: false
+	},
+	defaultTabWidth: 36,
+	enforcement: false,
+	hashValue: '',
+	saltValue: '',
+	formatting: false,
+	protectionType: 'NoProtection',
+	dontUseHTMLParagraphAutoSpacing: false,
+	styles: [
+		{
+			name: 'Normal',
+			type: 'Paragraph',
+			paragraphFormat: {
+				lineSpacing: 1.15,
+				lineSpacingType: 'Multiple',
+				listFormat: {}
+			},
+			characterFormat: {
+				fontFamily: 'Calibri'
+			},
+			next: 'Normal'
+		},
+		{
+			name: 'Heading 2',
+			type: 'Paragraph',
+			paragraphFormat: {
+				beforeSpacing: 2,
+				afterSpacing: 6,
+				outlineLevel: 'Level2',
+				listFormat: {}
+			},
+			characterFormat: {
+				fontSize: 13,
+				fontFamily: 'Calibri Light',
+				fontColor: '#2F5496'
+			},
+			basedOn: 'Normal',
+			link: 'Heading 2 Char',
+			next: 'Normal'
+		},
+		{
+			name: 'Heading 2 Char',
+			type: 'Character',
+			characterFormat: {
+				fontSize: 13,
+				fontFamily: 'Calibri Light',
+				fontColor: '#2F5496'
+			},
+			basedOn: 'Default Paragraph Font'
+		},
+		{
+			name: 'Default Paragraph Font',
+			type: 'Character',
+			characterFormat: {}
+		},
+		{
+			name: 'Heading 1',
+			type: 'Paragraph',
+			paragraphFormat: {
+				leftIndent: 0,
+				rightIndent: 0,
+				firstLineIndent: 0,
+				textAlignment: 'Left',
+				beforeSpacing: 12,
+				afterSpacing: 0,
+				lineSpacing: 1.0791666507720947,
+				lineSpacingType: 'Multiple',
+				outlineLevel: 'Level1',
+				listFormat: {}
+			},
+			characterFormat: {
+				fontSize: 16,
+				fontFamily: 'Calibri Light',
+				fontColor: '#2F5496'
+			},
+			basedOn: 'Normal',
+			link: 'Heading 1 Char',
+			next: 'Normal'
+		},
+		{
+			name: 'Heading 1 Char',
+			type: 'Character',
+			characterFormat: {
+				fontSize: 16,
+				fontFamily: 'Calibri Light',
+				fontColor: '#2F5496'
+			},
+			basedOn: 'Default Paragraph Font'
+		},
+		{
+			name: 'Heading 3',
+			type: 'Paragraph',
+			paragraphFormat: {
+				leftIndent: 0,
+				rightIndent: 0,
+				firstLineIndent: 0,
+				textAlignment: 'Left',
+				beforeSpacing: 2,
+				afterSpacing: 0,
+				lineSpacing: 1.0791666507720947,
+				lineSpacingType: 'Multiple',
+				outlineLevel: 'Level3',
+				listFormat: {}
+			},
+			characterFormat: {
+				fontSize: 12,
+				fontFamily: 'Calibri Light',
+				fontColor: '#1F3763'
+			},
+			basedOn: 'Normal',
+			link: 'Heading 3 Char',
+			next: 'Normal'
+		},
+		{
+			name: 'Heading 3 Char',
+			type: 'Character',
+			characterFormat: {
+				fontSize: 12,
+				fontFamily: 'Calibri Light',
+				fontColor: '#1F3763'
+			},
+			basedOn: 'Default Paragraph Font'
+		},
+		{
+			name: 'Heading 4',
+			type: 'Paragraph',
+			paragraphFormat: {
+				leftIndent: 0,
+				rightIndent: 0,
+				firstLineIndent: 0,
+				textAlignment: 'Left',
+				beforeSpacing: 2,
+				afterSpacing: 0,
+				lineSpacing: 1.0791666507720947,
+				lineSpacingType: 'Multiple',
+				outlineLevel: 'Level4',
+				listFormat: {}
+			},
+			characterFormat: {
+				italic: true,
+				fontFamily: 'Calibri Light',
+				fontColor: '#2F5496'
+			},
+			basedOn: 'Normal',
+			link: 'Heading 4 Char',
+			next: 'Normal'
+		},
+		{
+			name: 'Heading 4 Char',
+			type: 'Character',
+			characterFormat: {
+				italic: true,
+				fontFamily: 'Calibri Light',
+				fontColor: '#2F5496'
+			},
+			basedOn: 'Default Paragraph Font'
+		},
+		{
+			name: 'Heading 5',
+			type: 'Paragraph',
+			paragraphFormat: {
+				leftIndent: 0,
+				rightIndent: 0,
+				firstLineIndent: 0,
+				textAlignment: 'Left',
+				beforeSpacing: 2,
+				afterSpacing: 0,
+				lineSpacing: 1.0791666507720947,
+				lineSpacingType: 'Multiple',
+				outlineLevel: 'Level5',
+				listFormat: {}
+			},
+			characterFormat: {
+				fontFamily: 'Calibri Light',
+				fontColor: '#2F5496'
+			},
+			basedOn: 'Normal',
+			link: 'Heading 5 Char',
+			next: 'Normal'
+		},
+		{
+			name: 'Heading 5 Char',
+			type: 'Character',
+			characterFormat: {
+				fontFamily: 'Calibri Light',
+				fontColor: '#2F5496'
+			},
+			basedOn: 'Default Paragraph Font'
+		},
+		{
+			name: 'Heading 6',
+			type: 'Paragraph',
+			paragraphFormat: {
+				leftIndent: 0,
+				rightIndent: 0,
+				firstLineIndent: 0,
+				textAlignment: 'Left',
+				beforeSpacing: 2,
+				afterSpacing: 0,
+				lineSpacing: 1.0791666507720947,
+				lineSpacingType: 'Multiple',
+				outlineLevel: 'Level6',
+				listFormat: {}
+			},
+			characterFormat: {
+				fontFamily: 'Calibri Light',
+				fontColor: '#1F3763'
+			},
+			basedOn: 'Normal',
+			link: 'Heading 6 Char',
+			next: 'Normal'
+		},
+		{
+			name: 'Heading 6 Char',
+			type: 'Character',
+			characterFormat: {
+				fontFamily: 'Calibri Light',
+				fontColor: '#1F3763'
+			},
+			basedOn: 'Default Paragraph Font'
+		}
+	],
+	lists: [],
+	abstractLists: [],
+	comments: []
+};
