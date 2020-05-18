e0712b3ebca0b2ace5b76a67723e7688def11bed
ankitaShrma <ankita.sharma@zegal.com>
2020-05-18 15:28:32 +0545

    Update togglebookmark test

--- src/sfdt/__tests__/toggleBookmark.test.js
+++ src/sfdt/__tests__/toggleBookmark.test.js
@@ -25,7 +25,6 @@ const uuid = 'K1';
 const name = `${bookmarkType}::${uuid}`;
 
 describe('toggleBookmark', function() {
-
 	describe('Simple', function() {
 		test('Toggle off', function() {
 			const sfdt = getSFDT(inlines.concat(getBookmark(uuid, 'COND::')));
@@ -110,6 +109,7 @@ describe('toggleBookmark', function() {
 			const newSfdt = JSON.parse(JSON.stringify(nestedConditionListWithParentOnOneChildOff));
 			const nestedBookmarkSfdt = getSFDT(false, newSfdt);
 			const initialInlines = getFirstInlines(nestedBookmarkSfdt);
+
 			const parentBk = 'COND::dafe554d-08b5-463f-a40c-cf5e260be606';
 			const childBkOn = 'COND::5f1382fd-1d8e-475d-8439-8239b3e5397a';
 			const childBkOff = 'COND::cf6913ce-0e3b-4657-a164-88ab7c18a875';
@@ -167,37 +167,22 @@ describe('toggleBookmark', function() {
 
 			expect(firstInline[2].hasFieldEnd).toBe(true);
 			expect(lastInline[1].fieldType).toBe(1);
+			const newSfdt = JSON.parse(JSON.stringify(bookmarkStartEndingInDifferentInline));
+			const toggleOn = toggleBookmark(newSfdt, 'COND::9e7d0dc1-b9ed-4baa-9399-a4c4c9be96d4', true);
 
-			// const newSfdt = JSON.parse(JSON.stringify(bookmarkStartEndingInDifferentInline));
-			// const toggleOff = toggleBookmark(
-			// 	newSfdt,
-			// 	'COND::9e7d0dc1-b9ed-4baa-9399-a4c4c9be96d4',
-			// 	true
-			// );
-			//const firstInlineAfterToggle = getInline(toggleOff, 0);
-			//const lastInlineAfterToggle = getInline(toggleOff, 0, 2);
+			const firstInlineAfterToggle = getInline(toggleOn, 0);
 
-			//expect(firstInlineAfterToggle[2].hasFieldEnd).toBeUndefined();
-			//expect(lastInlineAfterToggle[1].fieldType).toBeUndefined();
+			expect(firstInlineAfterToggle.length).toEqual(firstInline.length);
 		});
 
 		test('Toggle off', function() {
 			let newSfdt = JSON.parse(JSON.stringify(bookmarkStartEndingInDifferentInline));
-			// const toggleOn = toggleBookmark(
-			// 	newSfdt,
-			// 	'COND::9e7d0dc1-b9ed-4baa-9399-a4c4c9be96d4',
-			// 	true
-			// );
-			// const firstInlineAfterToggle = getInline(toggleOn, 0);
-			// const lastInlineAfterToggle = getInline(toggleOn, 0, 2);
-
-			//expect(firstInlineAfterToggle[2].hasFieldEnd).toBeUndefined();
-			//expect(lastInlineAfterToggle[1].fieldType).toBeUndefined();
 
 			newSfdt = JSON.parse(JSON.stringify(bookmarkStartEndingInDifferentInline));
 			const toggleOff = toggleBookmark(newSfdt, 'COND::9e7d0dc1-b9ed-4baa-9399-a4c4c9be96d4', false);
 			const firstInlineAfterToggleOn = getInline(toggleOff, 0);
 			expect(firstInlineAfterToggleOn.length).toBe(1);
+			expect(firstInlineAfterToggleOn[0].text).toBe('Keep Distance with me.');
 		});
 	});
 
