c1ae6f3b33bb04c5c71954e3c20cb12df00fd36e
ankita.sharma <ankita.sharma@zegal.com>
2022-08-31 09:59:26 +0545

    restart block again in case of multiple loops

---
+++ src/sfdt/__tests__/fixtures/loopingMultipleLoops.js
@@ -0,0 +1,140 @@
+module.exports = {
+	sections: [
+		{
+			sectionFormat: {
+				pageWidth: 612,
+				pageHeight: 792,
+				leftMargin: 72,
+				rightMargin: 72,
+				topMargin: 72,
+				bottomMargin: 72,
+				differentFirstPage: true,
+				differentOddAndEvenPages: false,
+				headerDistance: 36,
+				footerDistance: 36,
+				bidi: false
+			},
+			blocks: [
+				{
+					paragraphFormat: {
+						textAlignment: 'Left',
+						styleName: 'Normal',
+						listFormat: {
+							listId: 1,
+							listLevelNumber: 0
+						}
+					},
+					characterFormat: {
+						fontSize: 11,
+						fontSizeBidi: 11
+					},
+					inlines: [
+						{
+							characterFormat: {},
+							bookmarkType: 0,
+							name: 'LOOP::34acf81b-2771-47d7-99ca-823b674e605c::customer.details'
+						},
+					
+						{
+							characterFormat: {},
+							bookmarkType: 0,
+							name: 'DATA::8ddd1249-f7cb-434e-8382-083e6e48bd4a::LOOP.name'
+						},
+						{
+							characterFormat: {
+								bidi: false
+							},
+							text: 'LOOP.name'
+						},
+						{
+							characterFormat: {},
+							bookmarkType: 1,
+							name: 'DATA::8ddd1249-f7cb-434e-8382-083e6e48bd4a::LOOP.name'
+						},
+						{
+							characterFormat: {},
+							bookmarkType: 1,
+							name: 'LOOP::34acf81b-2771-47d7-99ca-823b674e605c::customer.details'
+						}
+					]
+				},
+				{
+					paragraphFormat: {
+						leftIndent: 0,
+						textAlignment: 'Left',
+						styleName: 'Normal',
+						listFormat: {}
+					},
+					characterFormat: {},
+					inlines: [
+						{
+							characterFormat: {
+								bold: true,
+								fontSize: 11,
+								fontFamily: 'Arial',
+								boldBidi: true,
+								fontSizeBidi: 11,
+								fontFamilyBidi: 'Arial'
+							},
+							text: 'Background'
+						}
+					]
+				},
+				{
+					paragraphFormat: {
+						textAlignment: 'Left',
+						styleName: 'Normal',
+						listFormat: {
+							listId: 2,
+							listLevelNumber: 0
+						}
+					},
+					characterFormat: {
+						bold: false,
+						italic: false,
+						fontSize: 11,
+						fontFamily: 'Arial',
+						underline: 'None',
+						strikethrough: 'None',
+						baselineAlignment: 'Normal',
+						fontColor: '#00000000',
+						boldBidi: false,
+						italicBidi: false,
+						fontSizeBidi: 11,
+						fontFamilyBidi: 'Arial',
+						allCaps: false
+					},
+					inlines: [
+						{
+							characterFormat: {},
+							bookmarkType: 0,
+							name: 'LOOP::220a66b3-a219-4aae-8083-6d54ce1739b8::seller_details'
+						},
+						{
+							characterFormat: {},
+							bookmarkType: 0,
+							name: 'DATA::39625e94-31bd-4e06-b002-ed8a6f5773f8::LOOP.name1'
+						},
+						{
+							characterFormat: {
+								bidi: false
+							},
+							text: 'LOOP.name1'
+						},
+						{
+							characterFormat: {},
+							bookmarkType: 1,
+							name: 'DATA::39625e94-31bd-4e06-b002-ed8a6f5773f8::LOOP.name1'
+						},
+						{
+							characterFormat: {},
+							bookmarkType: 1,
+							name: 'LOOP::220a66b3-a219-4aae-8083-6d54ce1739b8::seller_details'
+						}
+					]
+				}
+			]
+		}
+	]
+};
+
